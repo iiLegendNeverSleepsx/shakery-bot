b00e6a6a2e33174a867afc76ef082452235475e7
lxphere <47260747+lxphere@users.noreply.github.com>
2019-02-02 14:29:41 -0500

    Update sannounce.js

--- commands/sannounce.js
+++ commands/sannounce.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
 	if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You cannot run this command. You must be a HR+!");
 	let embed = new Discord.RichEmbed()
 		.setTitle("Staff Announcement")
-		.setColor("#ffeb5c")
+		.setColor("#64bdff")
 		.setDescription(args.join(" "))
 		.setFooter(`Announcement made by ${message.author.tag}`, message.author.displayAvatarURL);
 	bot.channels.get("541009950697062430").send({embed: embed}).then(() => {
