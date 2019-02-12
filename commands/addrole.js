ecb25a4a7558a93c9324512eb3305a7d7aa47ace
lxphere <47260747+lxphere@users.noreply.github.com>
2019-02-12 17:19:51 -0500

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
+const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply("Invalid permissions! You need the `MANAGE_ROLES` permission!");
-  let rmember = message.mentions.members.first() || message.guild.members.get(args[0]);  
-  if(!rMember) return message.reply("Please mention a valid member of this server!");
+  let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);  if(!rMember) return message.reply("Please mention a valid member of this server!");
   let role = args.join(" ").slice(22);
   if(!role) return message.reply("Specify a role!");
   let gRole = message.guild.roles.find(`name`, role);
