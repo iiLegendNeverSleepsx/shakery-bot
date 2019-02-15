58072cf831e07fcb01d118e4e4284c9c3b72f26e
lxphere <47260747+lxphere@users.noreply.github.com>
2019-02-15 18:08:25 -0500

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -4,9 +4,10 @@ module.exports.run = async (bot, message, args) => {
 	if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You cannot run this command. You must be a HR+!");
 	let embed = new Discord.RichEmbed()
 		.setTitle("Shakery Announcement")
-		.setColor("#ffeb5c")
+		.setColor("#ff4b4b")
 		.setDescription(args.join(" "))
-		.setFooter(`Announcement made by ${message.author.tag}`, message.author.displayAvatarURL);
+		.setFooter(`Announcement made by ${message.author.tag}`, message.author.displayAvatarURL)
+ 		.setThumbnail("https://cdn.discordapp.com/attachments/541388616774057984/546105276491759658/asdas.png");
 	bot.channels.get("541365809931616287").send({embed: embed}).then(() => {
 		message.reply("Message sent!");
 	}).catch(() => {
