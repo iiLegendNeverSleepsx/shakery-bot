c84f61813dc91fe32de32949c6355505245f081c
lxphere <47260747+lxphere@users.noreply.github.com>
2019-02-16 11:55:52 -0500

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);  if(!rMember) return message.reply("Please mention a valid member of this server!");
   let role = args.join(" ").slice(1);
   if(!role) return message.reply("Specify a role!");
-  let gRole = message.guild.roles.find(role => role.name === 'name'
+  let gRole = message.guild.roles.find(role => role.name === 'name');
   if(!gRole) return message.reply("Couldn't find that role.");
 
   if(rMember.roles.has(gRole.id)) return message.reply("They already have that role!");
