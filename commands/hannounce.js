a6e2246e829b02bce50adaf4a9684b1082c4930e
lxphere <47260747+lxphere@users.noreply.github.com>
2019-02-16 11:26:54 -0500

    Update hannounce.js

--- commands/hannounce.js
+++ commands/hannounce.js
@@ -3,11 +3,12 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 	if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You cannot run this command. You must be a HR+!");
 	let embed = new Discord.RichEmbed()
-		.setTitle("Server Announcement")
-		.setColor("#ffeb5c")
+		.setTitle("Shakery Announcement")
+		.setColor("#ff4b4b")
 		.setDescription(args.join(" "))
-		.setFooter(`Announcement made by ${message.author.tag}`, message.author.displayAvatarURL);
-          bot.channels.get("541009925355077633").send("@here", {embed: embed}).then(() => {
+		.setFooter(`Announcement made by ${message.author.tag}`, message.author.displayAvatarURL)
+	        .setThumbnail("https://cdn.discordapp.com/attachments/541388616774057984/546105276491759658/asdas.png");
+          bot.channels.get("541365809931616287").send("@here", {embed: embed}).then(() => {
 		message.reply("Message sent!");
 	}).catch(() => {
 		message.reply("Something went wrong when announcing, please check my permissions and try again.");
