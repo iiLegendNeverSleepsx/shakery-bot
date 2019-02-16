fdd42f8367a6dfde9d162b88fb9b9d1d5059d028
lxphere <47260747+lxphere@users.noreply.github.com>
2019-02-16 11:25:23 -0500

    Update eannounce.js

--- commands/eannounce.js
+++ commands/eannounce.js
@@ -4,10 +4,11 @@ module.exports.run = async (bot, message, args) => {
 	if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You cannot run this command. You must be a HR+!");
 	let embed = new Discord.RichEmbed()
 		.setTitle("Server Announcement")
-		.setColor("#ffeb5c")
+		.setColor("#ff4b4b")
 		.setDescription(args.join(" "))
+	        .setThumbnail("https://cdn.discordapp.com/attachments/541388616774057984/546105276491759658/asdas.png")
 		.setFooter(`Announcement made by ${message.author.tag}`, message.author.displayAvatarURL);
-	bot.channels.get("541009925355077633").send("@everyone", {embed: embed}).then(() => {
+	bot.channels.get("541365809931616287").send("@everyone", {embed: embed}).then(() => {
 		message.reply("Message sent!");
 	}).catch(() => {
 		message.reply("Something went wrong when announcing, please check my permissions and try again.");
