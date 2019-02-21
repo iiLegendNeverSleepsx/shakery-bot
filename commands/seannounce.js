6f784683fb82529b5fd7552907fe08c265e32dae
lxphere <47260747+lxphere@users.noreply.github.com>
2019-02-21 16:16:34 -0500

    Update seannounce.js

--- commands/seannounce.js
+++ commands/seannounce.js
@@ -4,10 +4,11 @@ module.exports.run = async (bot, message, args) => {
 	if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You cannot run this command. You must be a HR+!");
 	let embed = new Discord.RichEmbed()
 		.setTitle("Staff Announcement")
-		.setColor("#64bdff")
+		.setColor("#ff4b4b")
 		.setDescription(args.join(" "))
-		.setFooter(`Announcement made by ${message.author.tag}`, message.author.displayAvatarURL);
-	bot.channels.get("541009950697062430").send("@everyone", {embed: embed}).then(() => {
+		.setFooter(`Announcement made by ${message.author.tag}`, message.author.displayAvatarURL)
+		.setThumbnail("https://cdn.discordapp.com/attachments/541388616774057984/546105276491759658/asdas.png");
+	bot.channels.get("548243923295010833").send("@everyone", {embed: embed}).then(() => {
 		message.reply("Message sent!");
 	}).catch(() => {
 		message.reply("Something went wrong when announcing, please check my permissions and try again.");
