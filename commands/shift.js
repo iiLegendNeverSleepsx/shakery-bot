3adeef2abc1f8bd81d2a72e4c4cb1b9b348f6419
lxphere <47260747+lxphere@users.noreply.github.com>
2019-02-21 16:31:14 -0500

    Create shift.js

---
+++ commands/shift.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+	if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You cannot run this command. You must be a HR+!");
+	let embed = new Discord.RichEmbed()
+		.setTitle("Shakery Shify")
+		.setColor("#63dfff")
+		.setDescription("Hello everyone! There is a shift going on at the cafe! Come on and join me for a luxrious time! https://www.roblox.com/games/2693333108/Shakery-V2")
+		.setFooter(`Shift hosted by ${message.author.tag}`, message.author.displayAvatarURL)
+	  .setThumbnail("https://cdn.discordapp.com/attachments/541388616774057984/546105276491759658/asdas.png");
+          bot.channels.get("548243671800217600").send("@here", {embed: embed}).then(() => {
+		message.reply("Message sent!");
+	}).catch(() => {
+		message.reply("Something went wrong when announcing, please check my permissions and try again.");
+	});
+};
+module.exports.help = {
+	name: "shift",
+	usage: "shift",
+	description: "Shift command.",
+	longdes: "Shift Command.",
+	mentionedperm: "KICK_MEMBERS",
+	category: "Utility"
+};
