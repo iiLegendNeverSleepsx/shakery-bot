db0060c6b6fd38c79d2bd2a737a594ef63a1aad9
lxphere <47260747+lxphere@users.noreply.github.com>
2019-02-22 18:17:32 -0500

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,7 +1,6 @@
 
 module.exports.run = async (bot, message, args) => {
-	if (!message.member.hasPermission("BAN_MEMBERS") || !message.member.hasPermission("ADMINISTRATOR"))
-		return message.reply("Insufficent permissions! You need to have the `BAN_MEMBERS` permission!");
+	if (!message.member.hasPermission("BAN_MEMBERS")) return message.reply("Insufficent permissions! You need to have the `BAN_MEMBERS` permission!");
 
 	let member = message.mentions.members.first() || message.guild.members.get(args[0]);
 	if (!member)
