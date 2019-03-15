a4725b1561fe5a075c3d9a70e572d9601971116e
lxphere <47260747+lxphere@users.noreply.github.com>
2019-03-15 16:09:40 -0400

    Update cointoss.js

--- commands/cointoss.js
+++ commands/cointoss.js
@@ -1,6 +1,8 @@
 module.exports = { run: async (bot, message) => {
 if (premiumaccess.includes(message.author.id)) {
-		message.reply(`You flipped ${(Math.floor(Math.random() * 2)) ? "heads" : "tails"}!`).catch(() => bot.safeSend(message, module.exports.help.name));
+		message.reply(`You flipped ${(Math.floor(Math.random() * 2)) ? "heads" : "tails"}!`);
+	}
+};
 module.exports.help = {
 	name: "cointoss",
 	usage: "cointoss",
