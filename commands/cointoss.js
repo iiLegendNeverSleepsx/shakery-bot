a93b4342e20f4f17f47d90b0083da4ca6d00af0a
lxphere <47260747+lxphere@users.noreply.github.com>
2019-03-16 12:03:08 -0400

    Update cointoss.js

--- commands/cointoss.js
+++ commands/cointoss.js
@@ -7,8 +7,8 @@ module.exports.run = async (bot, message) => {
 module.exports.help = {
 	name: "cointoss",
 	usage: "cointoss",
-	description: "A fun command that flips a coin/",
-	longdes: "Gives you either heads or tails..",
+	description: "A fun command that flips a coin.",
+	longdes: "Gives you either heads or tails.",
 	mentionedperm: "PREMIUMACCESS",
 	category: "Premium"
 };
