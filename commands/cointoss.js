57c516dada29606872c4ddd5a4fdb655291754e9
lxphere <47260747+lxphere@users.noreply.github.com>
2019-03-16 12:38:59 -0400

    Update cointoss.js

--- commands/cointoss.js
+++ commands/cointoss.js
@@ -1,5 +1,6 @@
 const bot = require("../bot.js");
 const client = bot;
+const client.premiumacess = require("../bot.js");
 module.exports.run = async (bot, message, args) => {
 if (client.premiumaccess.includes(message.author.role)) {
 message.reply(`You flipped ${(Math.floor(Math.random() * 2)) ? "heads" : "tails"}!`).catch(() => bot.safeSend(message, module.exports.help.name));
