3a1033fe3b2fc83f13d6e35f39f439434dfb4bbd
lxphere <47260747+lxphere@users.noreply.github.com>
2019-03-19 16:50:32 -0400

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,21 +1,21 @@
-
+const Discord = require("discord.js");
+const fs = require("fs");
 module.exports.run = async (bot, message, args) => {
-	 if(!message.member.hasPermission("BAN_MEMBERS") || !message.member.hasPermission("ADMINISTRATOR"))
- 		return message.reply("Insufficent permissions! You need to have the `BAN_MEMBERS` permission!");
-
-	let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-	if (!member)
-		return message.reply("Please mention a valid member of this server!");
-	if (!member.kickable)
-		return message.reply("I cannot ban this user! Do they have a higher role? **Do I have ban permissions**?");
-
-	let reason = args.slice(1).join(" ");
-	if (!reason) reason = "No reason provided";
-
-	await member.ban(reason)
-		.catch((error) => message.reply(`\nSorry ${message.author}, I couldn't ban because of: ${error}`));
-	message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
-};
+    if(!message.member.hasPermission("BAN_MEMBERS") || !message.member.hasPermission("ADMINISTRATOR"))
+      return message.reply("\nInsufficent permissions! You need to have the `BAN_MEMBERS` permission!");
+    
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("\nPlease mention a valid member of this server!");
+    if(!member.kickable) 
+      return message.reply("I cannot ban this user! Do they have a higher role? **Do I have ban permissions**?");
+    
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";
+    
+    await member.ban(reason)
+      .catch(error => message.reply(`\nSorry ${message.author}, I couldn't ban because of: ${error}`));
+    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
 
 module.exports.help = {
 	name: "ban",
